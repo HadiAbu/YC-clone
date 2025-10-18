116c291ec2676055da1d264acf14c833897ce833
HadiAbu <hadi.abuhamed@gmail.com>
2025-10-18 12:10:34 +0900

    adding more pages (about, users, analytics)

--- app/components/hello.tsx
+++ app/components/hello.tsx
@@ -6,7 +6,7 @@ export default function Hello() {
   console.log("Hello, i am a client compoenent");
   return (
     <>
-      <h2>Hello, Next.js!</h2>;
+      <h2>Hello, Next.js!</h2>
     </>
   );
 }
