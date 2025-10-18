2b0c4481a45f28b3bfc239e84290290ac84f5eef
HadiAbu <hadi.abuhamed@gmail.com>
2025-10-18 10:12:19 +0900

    first commit

---
+++ app/components/hello.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function Hello() {
+  console.log("Hello, i am a client compoenent");
+  return (
+    <>
+      <h2>Hello, Next.js!</h2>;
+    </>
+  );
+}
